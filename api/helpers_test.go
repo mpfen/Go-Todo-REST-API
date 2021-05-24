c7f122fe70ded295c2dcc538c5693427fd0c4f7a
mpfen <Michael@mpfen.de>
2021-05-24 16:06:29 +0200

    moved StubProjectStore to own file helpers_test.go

---
+++ api/helpers_test.go
@@ -0,0 +1,45 @@
+package api_test
+
+import (
+	"errors"
+
+	"github.com/mpfen/Go-Todo-REST-API/api/model"
+)
+
+// DB store stub for testing
+type StubProjectStore struct {
+	projects map[string]string
+}
+
+// creates a makeshift project struct to comply with ProjectStore interface
+func (s *StubProjectStore) GetProject(name string) model.Project {
+	project := model.Project{}
+	project.Name = s.projects[name]
+	return project
+}
+
+// creates a new project
+func (s *StubProjectStore) PostProject(name string) error {
+	if duplicate := s.projects[name]; duplicate != "" {
+		return errors.New("project already created")
+	}
+	s.projects[name] = name
+	return nil
+}
+
+// Returns an array of all projects
+func (s *StubProjectStore) GetAllProjects() []model.Project {
+	var projects []model.Project
+
+	for key := range s.projects {
+		projects = append(projects, model.Project{Name: key})
+	}
+
+	return projects
+}
+
+// Deletes a project from store
+func (s *StubProjectStore) DeleteProject(name string) error {
+	delete(s.projects, name)
+	return nil
+}
--- api/server_test.go
+++ api/server_test.go
@@ -3,7 +3,6 @@ package api_test
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -12,44 +11,6 @@ import (
 	api "github.com/mpfen/Go-Todo-REST-API/api"
 	"github.com/mpfen/Go-Todo-REST-API/api/model"
 )
-
-// DB store stub for testing
-type StubProjectStore struct {
-	projects map[string]string
-}
-
-// creates a makeshift project struct to comply with ProjectStore interface
-func (s *StubProjectStore) GetProject(name string) model.Project {
-	project := model.Project{}
-	project.Name = s.projects[name]
-	return project
-}
-
-// creates a new project
-func (s *StubProjectStore) PostProject(name string) error {
-	if duplicate := s.projects[name]; duplicate != "" {
-		return errors.New("project already created")
-	}
-	s.projects[name] = name
-	return nil
-}
-
-// Returns an array of all projects
-func (s *StubProjectStore) GetAllProjects() []model.Project {
-	var projects []model.Project
-
-	for key, _ := range s.projects {
-		projects = append(projects, model.Project{Name: key})
-	}
-
-	return projects
-}
-
-// Deletes a project from store
-func (s *StubProjectStore) DeleteProject(name string) error {
-	delete(s.projects, name)
-	return nil
-}
 
 // Tests for route GET /projects/{name}
 // todo update map to struct or array
